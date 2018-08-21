43d8f55cafa470f80da7f56625e58641ad2ae848
Franz Miltz <franz@miltz.me>
2018-08-21 20:01:12 +0200

    XPS Manjaro setup

--- XPS/i3/ws2.sh
+++ XPS/i3/ws2.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-i3-msg "workspace 2:terminal; append_layout ~/.config/i3/ws2.json" 
-termite --name "update" -e "tty-clock -scDd 5 -C 7" &
+i3-msg "workspace 2:shell; append_layout ~/.config/i3/ws2.json" 
+termite --name "update" -e "tty-clock -scDa 100000000 -C 2" &
 termite --name "terminal" -e fish &
 termite --name "ranger" -e ranger & 
---
+++ saveXPS.sh
@@ -0,0 +1,4 @@
+cp ~/.config/i3/* ~/dotfiles/XPS/i3/ 
+cp ~/.config/polybar/* ~/dotfiles/XPS/polybar/
+cp ~/.config/termite/* ~/dotfiles/termite/
+cp ~/.config/ranger/* ~/dotfiles/ranger/ -r
