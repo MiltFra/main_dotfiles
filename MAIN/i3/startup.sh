42740c9e5a0627bb07f9c9192c51b011fa6d79f5
Viktor Relda <franz@miltz.me>
2018-08-29 22:37:32 +0200

    Startup changed

--- MAIN/i3/startup.sh
+++ MAIN/i3/startup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 setxkbmap -layout us,de
 setxkbmap -option 'grp:win_space_toggle'
-i3-msg "workspace 2:shell; append_layout ~/.config/i3/ws2.json" 
 wal -R
+i3-msg "workspace 2:shell; append_layout ~/.config/i3/ws2.json" 
 termite --name "update" -e "tty-clock -scDa 100000000 -C 2" &
 termite --name "terminal" -e fish &
 termite --name "ranger" -e ranger &
@@ -12,3 +12,5 @@ firefox &
 thunderbird &
 spotify &
 polybar -r default 
+sleep 9
+wal -R
