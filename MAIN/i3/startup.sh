6f6f5d31fecd87109d69e9cb2783d7d205d101f6
Viktor Relda <franz@miltz.me>
2018-08-29 21:47:49 +0200

    Better PyWal integration

--- MAIN/i3/startup.sh
+++ MAIN/i3/startup.sh
@@ -2,6 +2,7 @@
 setxkbmap -layout us,de
 setxkbmap -option 'grp:win_space_toggle'
 i3-msg "workspace 2:shell; append_layout ~/.config/i3/ws2.json" 
+wal -R
 termite --name "update" -e "tty-clock -scDa 100000000 -C 2" &
 termite --name "terminal" -e fish &
 termite --name "ranger" -e ranger &
