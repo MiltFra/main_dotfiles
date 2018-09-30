85956d45f8b4423dfac18528a3c4b4658629d4b2
Franz Miltz <franz@miltz.me>
2018-09-30 09:19:19 +0200

    More WPGTK

--- i3/always.sh
+++ i3/always.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
-wpg -s $(cat ~/.config/bg)
 ~/.config/screenlayout.sh 
 compton 
 setxkbmap -layout us,de
 setxkbmap -option 'grp:win_space_toggle' 
 killall polybar
 polybar default -r
-wpg -s $(cat ~/.config/bg)
-sleep 2
-wpg -s $(cat ~/.config/bg)
-sleep 20
-wpg -s $(cat ~/.config/bg)
--- i3/startup.sh
+++ i3/startup.sh
@@ -5,12 +5,11 @@ i3-msg "workspace 2:shell; append_layout ~/.config/i3/ws2v2.json"
 termite --name "htop" -e "htop" &
 termite --name "ranger" -e ranger &
 sleep 1
-termite -c "wal -R" &
+termite &
 termite &
 code-oss &
 firefox &
 thunderbird &
 spotify &
 polybar -r default & 
-sleep 40
-wpg -s $(cat ~/.config/bg)
+/home/vrelda/.config/wpg/wp_init.sh
