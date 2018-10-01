01641de2f544533032f85627dfbde5b21f9105a5
Franz Miltz <franz@miltz.me>
2018-10-01 18:07:36 +0200

    Files removed

--- scripts/loadXPS.sh
+++
@@ -1,9 +0,0 @@
-cp ~/dotfiles/fonts/* ~/.fonts/
-cp ~/dotfiles/i3/*.* ~/.config/i3/
-cp ~/dotfiles/rofi/config ~/.config/rofi
-cp ~/dotfiles/i3/xps_config ~/.config/i3/config
-cp ~/dotfiles/polybar/xps_config ~/.config/polybar/config
-cp ~/dotfiles/fish ~/.config/ -r
-cp ~/dotfiles/ranger ~/.config/ -r
-cp ~/dotfiles/termite ~/.config/ -r
-cp ~/dotfiles/neofetch ~/.config/ -r
--- scripts/saveMAIN.sh
+++
@@ -1,9 +0,0 @@
-cp ~/.fonts/* ~/dotfiles/fonts/
-cp ~/.config/i3/*.* ~/dotfiles/i3/
-cp ~/.config/rofi/config ~/dotfiles/rofi/
-cp ~/.config/i3/config ~/dotfiles/i3/main_config
-cp ~/.config/polybar/config ~/dotfiles/polybar/main_config
-cp ~/.config/fish ~/dotfiles/ -r
-cp ~/.config/ranger ~/dotfiles/ -r
-cp ~/.config/termite ~/dotfiles/ -r
-cp ~/.config/neofetch ~/dotfiles/ -r
